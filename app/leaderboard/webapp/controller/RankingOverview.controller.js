ef53020237227845d4fc8526310dd27f6fc66898
Maksym Peksa <s152924@ap.be>
2026-01-15 16:26:12 +0000

    Add leaderboard

---
+++ app/leaderboard/webapp/controller/RankingOverview.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("apmorrowland.leaderboard.controller.RankingOverview", {
+        onInit() {
+        }
+    });
+});
