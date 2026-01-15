941d4b42d36235d26c65209c19a334fa4ef5f989
Maksym Peksa <s152924@ap.be>
2026-01-15 13:09:38 +0000

    feat: add Artist overview table with search functionality

--- app/artistmanagement/webapp/controller/ArtistOverview.controller.js
+++ app/artistmanagement/webapp/controller/ArtistOverview.controller.js
@@ -1,10 +1,44 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("apmorrowland.artistmanagement.controller.ArtistOverview", {
-        onInit() {
+        
+        onInit: function () {
+            // Initialisatie
+        },
+
+        // Zoekfunctie
+        onSearch: function (oEvent) {
+            var sQuery = oEvent.getParameter("query");
+            var oTable = this.byId("artistsTable");
+            var oBinding = oTable.getBinding("items");
+            
+            var aFilters = [];
+            if (sQuery) {
+                aFilters.push(new Filter("name", FilterOperator.Contains, sQuery));
+            }
+            
+            oBinding.filter(aFilters);
+        },
+
+        // Navigatie naar detail
+        onArtistPress: function (oEvent) {
+            var oItem = oEvent.getSource();
+            var oBindingContext = oItem.getBindingContext();
+            var sArtistId = oBindingContext.getProperty("ID");
+            
+            // Later: navigatie naar detail pagina
+            sap.m.MessageToast.show("Artiest ID: " + sArtistId);
+        },
+
+        // Nieuwe artiest toevoegen
+        onAddArtist: function () {
+            // Later: dialog openen
+            sap.m.MessageToast.show("Nieuwe artiest toevoegen...");
         }
     });
 });
